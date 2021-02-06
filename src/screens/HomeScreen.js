b4508e5fcfd59ebebcd57852776ce3198b2cf5b4
Amaury Soto <amaurysotog@gmail.com>
2021-02-06 18:21:46 -0500

    Responsive profile screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -24,32 +24,32 @@ const HomeScreen = () => {
        <Row
         title="Top Rated"
         fetchUrl={requests.fetchTopRated}
-        
+        isLargeRow
        />
        <Row
         title="Action Movies"
         fetchUrl={requests.fetchActionMovies}
-        
+        isLargeRow
        />
        <Row
         title="Comedy Movies"
         fetchUrl={requests.fetchComedyMovies}
-        
+        isLargeRow
        />
        <Row
         title="Horror Movies"
         fetchUrl={requests.fetchHorrorMovies}
-        
+        isLargeRow
        />
        <Row
         title="Romance Movies"
         fetchUrl={requests.fetchRomanceMovies}
-        
+        isLargeRow
        />
        <Row
         title="Documentaries"
         fetchUrl={requests.fetchDocumentaries}
-        
+        isLargeRow
        />
        
     </div>
